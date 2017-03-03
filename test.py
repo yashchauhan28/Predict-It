33e649f4da2e4d36cdb09ade1a9ab2a2ef2644f8
yashchauhan28 <yashchauhan281@gmail.com>
2017-03-03 15:54:57 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 30 14:13:13 2016
+
+@author: yashchauhan
+"""
+
+import sentiment_mod as s
+
+print(s.sentiment("This movie was awesome. The acting was great, plot was wonderful, and there were pythons...so yea!"))
+
+print(s.sentiment("This movie was utter junk. There were absolutely 0 pythons. i don't see what the point was at all. Horrible movie. 0/10"))
+
